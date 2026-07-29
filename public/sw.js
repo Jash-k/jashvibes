9bdaac507c7a7f4bcad22a3f56281bcfd3631700
Jash-k <dangerdreamer2010@gmail.com>
2026-07-29 12:54:16 +0530

    fix stremio route

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'jash-vibes-pwa-v1';
+const CACHE_VERSION = 'jash-vibes-pwa-v2';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const STATIC_PATHS = [
   '/manifest.webmanifest',
