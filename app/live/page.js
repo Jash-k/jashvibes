17aa34baca19c8dd176fd321cd1b567d0c4fb245
Jash-k <dangerdreamer2010@gmail.com>
2026-07-31 17:58:54 +0530

    add live source

--- app/live/page.js
+++ app/live/page.js
@@ -270,7 +270,6 @@ export default function LiveTVPage() {
             'time_and_duration',
             'spacer',
             'quality',
-            'picture_in_picture',
             'fullscreen',
           ],
           seekBarColors: {
