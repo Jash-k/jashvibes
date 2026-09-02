17af7e266d3a629d0b663a95d5e7055b6b1bc217
Jash-k <dangerdreamer2010@gmail.com>
2026-09-03 00:13:20 +0530

    fix match cc

--- app/sports/page.js
+++ app/sports/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import BrandLogo from '@/components/BrandLogo';
 import Icon from '@/components/Icons';
 import {
